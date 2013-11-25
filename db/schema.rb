a3b1444a611025c3ca0b7c2ba8b20545dc38f859
kimihito <tatsurotamashiro@gmail.com>
2013-11-25 11:57:22 +0900

    add column thumbnail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131121060155) do
+ActiveRecord::Schema.define(version: 20131125025646) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20131121060155) do
     t.string   "provider"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "thubmnail"
   end
 
 end
