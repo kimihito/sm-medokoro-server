d03dac054fd513c47c04200ac95808f639d2a7d6
kimihito <tatsurotamashiro@gmail.com>
2013-11-25 10:28:56 +0900

    spec validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ActiveRecord::Base
   attr_accessor :title, :url, :provider
   validates :title,:provider, :url, presence: true
-  validates :url,  format: { with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\Z/, on: :create}
   has_many :categories
 end
