047a1eaf3fddd5fdc53385a9c8fdfac7d34cf5f6
kimihito <tatsurotamashiro@gmail.com>
2013-12-14 23:35:49 +0900

    take 4 movies in top page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,10 @@ class HomeController < ApplicationController
 
     # TODO: まとめて書けそう
     @movie_providers = %w{youtube niconico vimeo fc2}
-    @youtube_movies = movies.where(provider: "youtube").order("created_at DESC")
-    @niconico_movies = movies.where(provider: "niconico").order("created_at DESC")
-    @vimeo_movies = movies.where(provider: "vimeo").order("created_at DESC")
-    @fc2_movies = movies.where(provider: "fc2").order("created_at DESC")
+    @youtube_movies = movies.where(provider: "youtube").order("created_at DESC").take(4)
+    @niconico_movies = movies.where(provider: "niconico").order("created_at DESC").take(4)
+    @vimeo_movies = movies.where(provider: "vimeo").order("created_at DESC").take(4)
+    @fc2_movies = movies.where(provider: "fc2").order("created_at DESC").take(4)
 
-  end
-
-  def all_movies
-    # @movie = Movie.where(provider: "vimeo").take(25)
-    @movie = Movie.where(provider: "niconico").take(25)
-    render json: @movie, callback: params[:callback]
   end
 end
