d1a32747869eee56ae6047477a7a19bc212eb56a
kimihito <tatsurotamashiro@gmail.com>
2013-12-22 09:21:07 +0900

    fix syntax error.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,5 @@ class HomeController < ApplicationController
     start_date = Date.yesterday + 9.hours
     end_date   = start_date + 1.day
     @movies     = Movie.where(created_at: start_date..end_date).order('created_at DESC')
-    end
   end
 end
