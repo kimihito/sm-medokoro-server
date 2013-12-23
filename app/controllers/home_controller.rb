616d40d4178daad0eabdd319d5e5b70c0f88abaf
kimihito <tatsurotamashiro@gmail.com>
2013-12-23 18:23:31 +0900

    存在しない日付、未来の日付、リストが空である日付を指定したときにerrorsディレクトリ内にrenderするようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
   def index
     start_date = Date.yesterday + 9.hours
     end_date   = start_date + 1.day
-    @movies     = Movie.where(created_at: start_date..end_date).order('created_at DESC')
+    @movies    = Movie.where(created_at: start_date..end_date).order('created_at DESC')
+    respond_to do |format|
+      if @movies.empty?
+        format.html {render template: 'errors/empty'}
+      end
+      format.html
+    end
   end
 end
